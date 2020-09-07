9c8074159ed43ccc158b7771a5d9ab60a1402ae8
Arnar Bjarki <arnar2302@gmail.com>
2020-09-07 19:25:34 +0000

    veit ekki eh

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+   
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/monkey")
+def monkey():
+    return render_template('monkey.html')
+
+if __name__ == '__main__' :
+    app.run(debug=True, use_reloader=True)
